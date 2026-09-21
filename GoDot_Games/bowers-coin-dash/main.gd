749eeb41f485ba546571b67eb90c9486cc56d734
Jenn Bowers <jbowers80@ivytech.edu>
2026-09-21 17:00:54 -0400

    Finished Chapter 2 Coin Dash Tutorial
    upload 1

--- GoDot_Games/bowers-coin-dash/main.gd
+++ GoDot_Games/bowers-coin-dash/main.gd
@@ -2,6 +2,7 @@ extends Node
 
 @export var coin_scene : PackedScene
 @export var playtime = 30
+@export var powerup_scene : PackedScene
 
 var level = 1
 var score = 0
@@ -35,12 +36,16 @@ func spawn_coins():
 		c.screensize = screensize
 		c.position = Vector2(randi_range(0, screensize.x),
 			randi_range(0, screensize.y))
+	$LevelSound.play()
 			
 func _process(delta):
 	if playing and get_tree().get_nodes_in_group("coins").size() == 0:
 		level += 1
 		time_left += 5
 		spawn_coins()
+		$PowerupTimer.wait_time = randf_range(5, 10)
+		$PowerupTimer.start()
+	
 
 func _on_game_timer_timeout():
 	time_left -= 1
@@ -51,9 +56,17 @@ func _on_game_timer_timeout():
 func _on_player_hurt():
 	game_over()
 	
-func _on_player_pickup():
-	score += 1
-	$HUD.update_score(score)
+func _on_player_pickup(type):
+	match type:
+		"coin":
+			$CoinSound.play()
+			score += 1
+			$HUD.update_score(score)
+		"powerup":
+			$PowerupSound.play()
+			time_left += 5
+			$HUD.update_timer(time_left)
+	
 	
 func game_over():
 	playing = false
@@ -61,6 +74,15 @@ func game_over():
 	get_tree().call_group("coins", "queue_free")
 	$HUD.show_game_over()
 	$Player.die()
+	$EndSound.play()
 
 func _on_hud_start_game():
 	new_game()
+
+
+func _on_powerup_timer_timeout():
+	var p = powerup_scene.instantiate()
+	add_child(p)
+	p.screensize = screensize
+	p.position = Vector2(randi_range(0, screensize.x),
+	randi_range(0, screensize.y))
--- GoDot_Games/bowers-coin-dash/powerup.gd
+++ GoDot_Games/bowers-coin-dash/powerup.gd
@@ -9,14 +9,10 @@ func pickup():
 	tw.tween_property(self, "modulate:a", 0.0, 0.3)
 	await tw.finished
 	queue_free()
-	
-func _ready():
-		$Timer.start(randf_range(3,8))
 
-func _on_timer_timeout():
-		$AnimatedSprite2D.frame = 0
-		$AnimatedSprite2D.play()
-		
+func _on_lifetime_timeout() -> void:
+	queue_free()
+	
 func _on_area_entered(area):
 	if area.is_in_group("obstacles"):
 		position = Vector2(randi_range(0, screensize.x),
